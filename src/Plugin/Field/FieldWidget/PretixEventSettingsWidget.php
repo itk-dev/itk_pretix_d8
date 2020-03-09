a9f6dbe0af34834b21feb609ccf86c8171f47aee
Mikkel Ricky <rimi@aarhus.dk>
2020-03-09 10:40:17 +0100

    HRIN-466: Added pretix links on event node

--- src/Plugin/Field/FieldWidget/PretixEventSettingsWidget.php
+++ src/Plugin/Field/FieldWidget/PretixEventSettingsWidget.php
@@ -50,6 +50,13 @@ class PretixEventSettingsWidget extends WidgetBase {
       $emptyOption = NULL;
     }
 
+    /** @var \Drupal\itk_pretix\Pretix\EventHelper $eventHelper */
+    $eventHelper = \Drupal::service('itk_pretix.event_helper');
+    $element['data'] = [
+      '#theme' => 'itk_pretix_event_data',
+      '#data' => $eventHelper->loadPretixEventInfo($node) ?? [],
+    ];
+
     $element['template_event'] = [
       '#type' => 'select',
       '#options' => $templateEventOptions,
@@ -73,10 +80,7 @@ class PretixEventSettingsWidget extends WidgetBase {
     if (1 === $this->fieldDefinition->getFieldStorageDefinition()->getCardinality()) {
       $element += [
         '#type' => 'details',
-        // Collapse when editing a node.
-        '#open' => NULL === $node->id()
-          // Or when some value is not valid.
-        || count($items[$delta]->validate() ?? []) > 0,
+        '#open' => TRUE,
       ];
     }
 
